4f76a66698d684bba8a841d9845fcabebbcf904b
ytua369 <y.m.36912@gmail.com>
2024-05-12 05:51:31 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :books
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  # get 'homes/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
