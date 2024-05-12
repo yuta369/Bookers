0e54351f9d9ac8f3a426bcf1b912cd8f2871bd23
ytua369 <y.m.36912@gmail.com>
2024-05-12 07:06:04 +0000

    [Add] Topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :books
+  root "homes#top"
   # get 'books/new'
   # get 'books/index'
   # get 'books/show'
   # get 'books/edit'
-  # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
