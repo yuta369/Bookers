24290c6b52fef85fe38953d0ed0410e47de6f9c4
ytua369 <y.m.36912@gmail.com>
2024-05-12 16:45:16 +0000

    [Add] バリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,48 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(list_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
-    @lists = List.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
-
+  
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(list_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+  
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
